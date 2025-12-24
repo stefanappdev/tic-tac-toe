9c9488e4ef19137b42dc7eafd0f7ff123bf83b6b
stefanappdev <stevendking@protonmail.com>
2025-12-24 12:23:04 -1100

    reorganized initial files for project December 24,2025

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+           hello world
+      </div>
+      
+    </>
+  )
+}
+
+export default App
